7c2074db7cd3c1a8e43e671b31a71f062e9780df
heyyousa <530877640@qq.com>
2022-03-01 22:53:47 +0800

    vue-project

---
+++ hip-demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <login></login>
+  </div>
+</template>
+
+<script>
+import login from "./components/login.vue";
+
+export default {
+  components: {
+    login: login,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ hip-demo/src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="container">
+    <el-form class="loginform">
+      <el-form-item>
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang='less' scoped>
+#container {
+  background-color: #eee;
+  width: 300px;
+  height: 300px;
+  position: relative;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.loginform {
+  position: absolute;
+  width: 100%;
+  bottom: 0px;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+</style>
