3cc34a535cd34b7e57466ac3f698275a366e4b35
wanghao <530877640@qq.com>
2022-03-04 15:54:44 +0800

    vue-project

--- hip-demo/src/views/sign.vue
+++ hip-demo/src/views/sign.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="vbody">
     <div class="container">
+      <div class="iconbox">
+        <el-avatar :size="150" :src="circleUrl" class="icon"></el-avatar>
+        <div class="suf-title">选择头像</div>
+      </div>
       <el-form
         :model="signform"
         class="signform"
-        label-width="80px"
-        :rules="signrules"
+        label-width="70px"
         ref="signform"
       >
         <el-form-item label="账号" prop="uid">
           <el-input
             v-model="signform.uid"
             placeholder="不超过15位，不允许包含中文，推荐使用本人工号"
+          ></el-input>
+        </el-form-item>
+        <el-form-item label="昵称" prop="username">
+          <el-input
+            v-model="signform.username"
+            placeholder="不超过15个字"
           ></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
@@ -21,11 +30,16 @@
             type="password"
           ></el-input>
         </el-form-item>
-        <el-form-item label="昵称" prop="username">
+        <el-form-item label="确认密码" prop="password">
           <el-input
-            v-model="signform.username"
-            placeholder="不超过15个字"
+            v-model="signform.password"
+            placeholder="不超过15位"
+            type="password"
           ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">注册</el-button>
+          <el-button type="info">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -36,6 +50,9 @@
 export default {
   data() {
     return {
+      // 加载本地图片资源用require函数
+      circleUrl: require("../assets/logo.png"),
+
       signform: {
         uid: "",
         password: "",
@@ -83,10 +100,25 @@ export default {
   background-color: #3f51b5;
 }
 
+.icon {
+  position: absolute;
+  left: 50%;
+  top: -75px;
+  transform: translate(-50%);
+  cursor: pointer;
+}
+
+.suf-title {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  top: 80px;
+}
+
 .container {
   background-color: #eee;
   width: 450px;
-  height: 500px;
+  height: 420px;
   position: relative;
   left: 50%;
   top: 50%;
